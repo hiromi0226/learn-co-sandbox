c53cde2d8354de8da59b989fcb9484c2be7bf3fd
hiromi0226 <hiromi.26ha@gmail.com>
2018-09-21 14:09:10 +0000

    automatically backed up by learn

--- mathsucks.rb
+++ mathsucks.rb
@@ -11,7 +11,7 @@ class Math
   end 
 end 
 
-math1 = math.new("numbers", "letters", "angles")
+math1 = Math.new("numbers", "letters", "angles")
 math1.activity = "fun"
   
 puts "There are three main types of High School math. The first is Calculus, who's main idea is #{math1.calculus}. Then there is Algebra, in which #{math1.algebra} is the main discussion, similar to Geometry and its #{math1.geometry}. However, none of these are the kind of activity you'd call ' #{math1.activity} '"
